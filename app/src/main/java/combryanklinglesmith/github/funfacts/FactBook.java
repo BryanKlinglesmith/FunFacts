87057406e73238b1968315d131c7a44df5048d3e
bryan klinglesmith <b.klinglesmith@yahoo.com>
2019-01-19 11:21:33 -0500

    Added some new facts to the app and added a second button with no function yet.

--- app/src/main/java/combryanklinglesmith/github/funfacts/FactBook.java
+++ app/src/main/java/combryanklinglesmith/github/funfacts/FactBook.java
@@ -15,7 +15,14 @@ class FactBook {
             "Some penguins can leap 2-3 meters out of the water.",
             "On average, it takes 66 days to form a new habit.",
             "Mammoths still walked the earth when the Great Pyramid was being built.",
-            "Treehouse is not actually in a tree."};
+            "Treehouse is not actually in a tree.",
+            "Kasie is a very cool person lol.",
+            "Aunt Nancy said Kasie is fake news",
+            "Lindsey said hit or miss. I guess they never miss huh",
+            "In 1642 Dutch explorer Abel Tasman sailed all the way around Australia and never saw it.",
+            "Amelia Airhart was the first person to fly solo from Hawaii to the United States.",
+            "Cap’n Crunch’s full name is Horatio Magellan Crunch.",
+            ""};
 
     // Methods - Actions the object can take
     String getFact() {
